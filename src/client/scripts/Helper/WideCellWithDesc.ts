aff110e198d88b0dbc4bbec35c417b8497cfc485
Ionut Opria <opriaionut14@gmail.com>
2023-08-15 13:57:12 +0300

    Be able to minimize special skills

--- src/client/scripts/Helper/WideCellWithDesc.ts
+++ src/client/scripts/Helper/WideCellWithDesc.ts
@@ -1,5 +1,8 @@
 export class WideCellWithDesc
 {
+    private _cellDesc: HTMLDivElement;
+    private _isVisible: boolean = false;
+
     constructor(parentNode: HTMLDivElement, cellID: string, title: string, description: string)
     {
         let cellDiv = document.createElement("div");
@@ -12,9 +15,22 @@ export class WideCellWithDesc
         cellTitle.innerHTML = title;
         cellDiv.appendChild(cellTitle);
 
-        let cellDesc = document.createElement("div");
-        cellDesc.className = "wideCellWithDescText";
-        cellDesc.innerHTML = description;
-        cellDiv.appendChild(cellDesc);
+        let cellBtn = document.createElement("button");
+        cellBtn.className = "wideCellWithDescBtn";
+        cellBtn.innerHTML = "^"
+        cellBtn.onclick = () => { this.toggleVisibility(); };
+        cellDiv.appendChild(cellBtn);
+
+        this._cellDesc = document.createElement("div");
+        this._cellDesc.className = "wideCellWithDescText";
+        this._cellDesc.innerHTML = description;
+        this._cellDesc.style.display = "none";
+        cellDiv.appendChild(this._cellDesc);
+    }
+
+    public toggleVisibility()
+    {
+        this._isVisible = !this._isVisible;
+        this._cellDesc.style.display = this._isVisible ? "block" : "none";
     }
 }
