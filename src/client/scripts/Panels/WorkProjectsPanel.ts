fd69d45262b917a3b28c4cb7c99c70b7180fa8ab
Ionut Opria <opriaionut14@gmail.com>
2023-09-10 14:58:40 +0300

    Fix broken galleries

--- src/client/scripts/Panels/WorkProjectsPanel.ts
+++ src/client/scripts/Panels/WorkProjectsPanel.ts
@@ -28,11 +28,11 @@ export class WorkProjectsPanel
             _description: `<br>
             Planetquest is a Blockchain game in which you can explore procedurally generated planets dirrectly in your browser. I was one of the first persons that joined the team and played a big role during the entire development of the project.<br><br>
             Key contributions: <br>
-            &#149; Developed a 3D engine that is able to procedurally generate planets dirrectly in the browser. <br>
-            &#149; Played a big role in the gameplay aspect of the project, by developing a lot of the core gameplay mechanics. <br>
-            &#149; Played a big role in each of the product launches. <br> 
-            &#149; Helped out in building a procedural volumetric terrain system that is also able to also generate underground caves. <br>
-            &#149; Currently helping out on the engine development side, by creating the tools that are required to run the game in the most optimal manner.`,
+            <b>&#149;</b> Developed a 3D engine that is able to procedurally generate planets dirrectly in the browser. <br>
+            <b>&#149;</b> Played a big role in the gameplay aspect of the project, by developing a lot of the core gameplay mechanics. <br>
+            <b>&#149;</b> Played a big role in each of the product launches. <br> 
+            <b>&#149;</b> Helped out in building a procedural volumetric terrain system that is also able to also generate underground caves. <br>
+            <b>&#149;</b> Currently helping out on the engine development side, by creating the tools that are required to run the game in the most optimal manner.`,
             _tags: ["WebGL", "Typescript", "C++", "Procedural", "Custom Engine"],
             _tagColors: [tagColors.language, tagColors.language, tagColors.language, tagColors.gameType, tagColors.software],
             _moreDetailsPage: "https://planetquest.io",
