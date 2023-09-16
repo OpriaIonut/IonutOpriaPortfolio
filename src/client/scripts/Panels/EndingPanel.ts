f59e5dca97726ff7ddf742563909731b33b6fcab
Ionut Opria <opriaionut14@gmail.com>
2023-09-16 10:42:31 +0300

    Added footer

--- src/client/scripts/Panels/EndingPanel.ts
+++ src/client/scripts/Panels/EndingPanel.ts
@@ -16,9 +16,47 @@ export class EndingPanel
         text.id = "endingPanelText";
         text.innerHTML = `
         Thank you for spending the time to look through my portfolio!<br>
-        In case you also want to take a look at my CV, you can download it <a href = 'CV Ionut Opria.pdf' download>here</a><br>
         In case you want to contact me you can do so here: opriaionut@gmailcom<br>
         `;
         parentNode.appendChild(text);
+
+        let linksParent = document.createElement("div");
+        linksParent.id = "linksParent";
+        parentNode.appendChild(linksParent);
+
+        this.createCellLink(linksParent, "./images/icons/linkedin.jpg", "https://www.linkedin.com/in/ionut-opria-6164b5150/");
+        this.createCellLink(linksParent, "./images/icons/github.jpg", "https://github.com/OpriaIonut");
+        this.createCellLink(linksParent, "./images/icons/itch.jpg", "https://kirirato.itch.io");
+        this.createCellLink(linksParent, "./images/icons/artstation.jpg", "https://www.artstation.com/kirirato16");
+        this.createCellLink(linksParent, "./images/icons/twitter.jpg", "https://twitter.com/Kirirato");
+        this.createCellLink(linksParent, "./images/icons/PDF.jpg", "CV");
+
+        let copyright = document.createElement("div");
+        copyright.id = "copyright";
+        copyright.innerHTML = "Copyright © 2023 by Ionu&#x021b; Opria<br>All content and trademarks property of their respective owners";
+        parentNode.appendChild(copyright);
+    }
+
+    private createCellLink(parentNode: HTMLElement, imageSrc: string, link: string)
+    {
+        let cell = document.createElement("img");
+        cell.className = "homePageReferalLink";
+        cell.src = imageSrc;
+        if(link == "CV")
+            cell.onclick = () => { this.downloadFile("CV Ionut Opria.pdf", "CV Ionut Opria.pdf"); };
+        else
+            cell.onclick = () => { window.open(link, '_blank'); };
+        cell.style.cursor = "pointer";
+        parentNode.appendChild(cell);
+    }
+
+    private downloadFile(filePath: string, fileName: string) 
+    {
+        const link = document.createElement('a');
+        link.href = filePath;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 }
