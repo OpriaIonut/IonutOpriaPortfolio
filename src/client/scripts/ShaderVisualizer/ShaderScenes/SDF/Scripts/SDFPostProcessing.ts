50ec7ecc9b6dd110b5b12fdea0bf4e0a1825cfec
OpriaIonut <opriaionut14@gmail.com>
2026-05-11 23:40:49 +0300

    Working SDF shapes

--- src/client/scripts/ShaderVisualizer/ShaderScenes/SDF/Scripts/SDFPostProcessing.ts
+++ src/client/scripts/ShaderVisualizer/ShaderScenes/SDF/Scripts/SDFPostProcessing.ts
@@ -1,7 +1,28 @@
-import { ShaderMaterial } from "three"
+import { Color, Matrix4, ShaderMaterial, Texture, Vector2, Vector3 } from "three"
+
+export declare type SDFData = {
+    pos: Vector3,
+    size: number,
+    color: Color
+}
 
 export declare type SDFPostProcessingUniforms = {
+    u_ScreenResolution: { value: Vector2 },
+    u_CameraPos: { value: Vector3 },
+    u_CameraMatrixWorld: { value: Matrix4 },
+    u_ProjectionMatrixInverse: { value: Matrix4 },
 
+    u_LightDir: { value: Vector3 },
+    u_AmbientIntensity: { value: number },
+    u_SceneColor: { value: Color },
+
+    u_SphereData: { value: SDFData },
+    u_BoxData: { value: SDFData },
+    u_CapsuleData: { value: SDFData },
+
+    u_ShapeSmoothness: { value: number },
+
+    u_DiffuseTex: { value: Texture | null }
 }
 
 export class SDFPostProcessing
@@ -9,9 +30,7 @@ export class SDFPostProcessing
     public static createPass(params: SDFPostProcessingUniforms): ShaderMaterial
     {
         return new ShaderMaterial({
-            uniforms: {
-                u_DiffuseTex: { value: null },
-            },
+            uniforms: params,
             vertexShader: sdfPostProcessingVert,
             fragmentShader: sdfPostProcessingFrag
         });
@@ -24,14 +43,127 @@ varying vec2 v_UV;
 void main()
 {
     v_UV = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    gl_Position = vec4(position.xy, 0.0, 1.0);
 }
 `;
 
 const sdfPostProcessingFrag = `
+#define MAX_STEPS 128
+#define SURFACE_EPSILON 0.001
+#define MAX_DISTANCE 100.0
+
 varying vec2 v_UV;
 
+uniform vec2 u_ScreenResolution;
+uniform vec3 u_CameraPos;
+uniform mat4 u_CameraMatrixWorld;
+uniform mat4 u_ProjectionMatrixInverse;
+
+uniform vec3 u_LightDir;
+uniform float u_AmbientIntensity;
+uniform vec3 u_SceneColor;
+
+struct SDFData
+{
+    vec3 pos;
+    float size;
+    vec3 color;
+};
+
+uniform SDFData u_SphereData;
+uniform SDFData u_BoxData;
+uniform SDFData u_CapsuleData;
+
+uniform float u_ShapeSmoothness;
+
 uniform sampler2D u_DiffuseTex;
+
+
+float sphereSDF(vec3 samplePoint, vec3 pos, float radius)
+{
+    return length(samplePoint - pos) - radius;
+}
+
+float boxSDF(vec3 samplePoint, vec3 pos, vec3 size)
+{
+    vec3 offset = abs(samplePoint - pos) - size;
+    float unsignedDist = length(max(offset, 0.0));
+    float distInsideBox = min(max(offset.x, max(offset.y, offset.z)), 0.0);
+    return unsignedDist + distInsideBox;
+}
+
+float capsuleSDF(vec3 samplePoint, vec3 capsuleCenter, vec3 topOffset, vec3 bottomOffset, float radius)
+{
+    vec3 top = capsuleCenter + topOffset;
+    vec3 bottom = capsuleCenter + bottomOffset;
+
+    vec3 ab = top - bottom;
+    float t = dot(samplePoint - bottom, ab) / dot(ab, ab);
+    t = clamp(t, 0.0, 1.0);
+
+    vec3 closestPoint = bottom + t * ab;
+    return length(samplePoint - closestPoint) - radius;
+}
+
+void smoothMin(float a, float b, vec3 aColor, vec3 bColor, float smoothness, out float dist, out vec3 color)
+{
+    float h = clamp(0.5 + 0.5 * (b - a) / smoothness, 0.0, 1.0);
+    dist = mix(b, a, h) - smoothness * h * (1.0 - h);
+    color = mix(bColor, aColor, h);
+}
+
+void sceneSDF(vec3 samplePoint, float smoothness, out float dist, out vec3 color)
+{
+    float sphere = sphereSDF(samplePoint, u_SphereData.pos, u_SphereData.size);
+    float box = boxSDF(samplePoint, u_BoxData.pos, vec3(u_BoxData.size, u_BoxData.size, u_BoxData.size));
+    float capsule = capsuleSDF(samplePoint, u_CapsuleData.pos, vec3(-1.0, 1.0, 0.0), vec3(1.0, -1.0, 0.0), u_CapsuleData.size);
+
+    smoothMin(box, capsule, u_BoxData.color, u_CapsuleData.color, smoothness, dist, color);
+    smoothMin(dist, sphere, color, u_SphereData.color, smoothness, dist, color);
+}
+
+vec3 calculateSceneSDFNormal(vec3 samplePoint, float smoothness)
+{
+    float e = 0.001;
+    float dx0, dx1, dy0, dy1, dz0, dz1;
+    vec3 tempColor;
+
+    sceneSDF(samplePoint + vec3(e, 0.0, 0.0), smoothness, dx0, tempColor);
+    sceneSDF(samplePoint - vec3(e, 0.0, 0.0), smoothness, dx1, tempColor);
+
+    sceneSDF(samplePoint + vec3(0.0, e, 0.0), smoothness, dy0, tempColor);
+    sceneSDF(samplePoint - vec3(0.0, e, 0.0), smoothness, dy1, tempColor);
+
+    sceneSDF(samplePoint + vec3(0.0, 0.0, e), smoothness, dz0, tempColor);
+    sceneSDF(samplePoint - vec3(0.0, 0.0, e), smoothness, dz1, tempColor);
+
+    float dx = dx0 - dx1;
+    float dy = dy0 - dy1;
+    float dz = dz0 - dz1;
+
+    return normalize(vec3(dx, dy, dz));
+}
+
+float raymarch(vec3 rayOrigin, vec3 rayDir, float smoothness, out vec3 color)
+{
+    float distTravelled = 0.0;
+    float sceneDist = 0.0;
+
+    for(int index = 0; index < MAX_STEPS; ++index)
+    {
+        vec3 samplePoint = rayOrigin + rayDir * distTravelled;
+        sceneSDF(samplePoint, smoothness, sceneDist, color);
+
+        if(sceneDist < SURFACE_EPSILON)
+            return distTravelled;
+
+        distTravelled += sceneDist;
+        if(distTravelled > MAX_DISTANCE)
+            break;
+    }
+    return -1.0;
+}
+    
 
 void main()
 {
@@ -39,6 +171,35 @@ void main()
 
     //Gamma correction
     vec3 colorOut = pow(sceneColor, vec3(0.4545)); //0.4545 = 1.0 / 2.2
+    vec3 backgroundColor = pow(u_SceneColor, vec3(0.4545)); //0.4545 = 1.0 / 2.2
+
+    float diff = length(colorOut - backgroundColor);
+    float mask = step(0.01, diff);
+    colorOut *= mask;
+
+
+    //Raymarch
+    vec2 raymarchUV = v_UV * 2.0 - 1.0;
+    vec4 target = u_ProjectionMatrixInverse * vec4(raymarchUV, 1.0, 1.0);
+    vec3 rayDir = normalize(target.xyz / target.w);
+    rayDir = normalize((u_CameraMatrixWorld * vec4(rayDir, 0.0)).xyz);
+
+    vec3 color = vec3(0.0);
+    float sceneDist = raymarch(u_CameraPos, rayDir, u_ShapeSmoothness, color);
+
+    vec3 sdfColor = vec3(0.0);
+
+    if(sceneDist > 0.0)
+    {
+        vec3 hitPos = u_CameraPos + rayDir * sceneDist;
+        vec3 normal = calculateSceneSDFNormal(hitPos, u_ShapeSmoothness);
+        float diffuse = max(dot(normal, -u_LightDir), 0.0);
+
+        sdfColor = color * vec3(diffuse * 0.5 + u_AmbientIntensity);
+    }
+    float sdfMask = 1.0 - step(0.01, length(sdfColor));
+
+    colorOut += backgroundColor * sdfMask + sdfColor;
 
     gl_FragColor = vec4(colorOut, 1.0);
 }
--- src/client/scripts/ShaderVisualizer/ShaderScenes/SDF/ShaderSceneSDF.ts
+++ src/client/scripts/ShaderVisualizer/ShaderScenes/SDF/ShaderSceneSDF.ts
@@ -1,9 +1,12 @@
-import { AmbientLight, Color, DirectionalLight, PerspectiveCamera, Scene, TextureLoader, Vector3 } from "three";
+import { AmbientLight, BoxGeometry, Color, DirectionalLight, Matrix4, Mesh, MeshStandardMaterial, Object3D, PerspectiveCamera, Scene, TextureLoader, Vector2, Vector3, WebGLRenderer } from "three";
 import { ShaderVisualizer } from "../../ShaderVisualizer";
 import { DebugUI } from "../../../ThreeVisualizer/DebugGUI";
 import { IShaderScene } from "../IShaderScene";
 import { SDFPostProcessing, SDFPostProcessingUniforms } from "./Scripts/SDFPostProcessing";
 import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FreeFlyCamera } from "../../../ThreeVisualizer/FreeFlyCamera";
 
 //Handles high-level management of the scene and it's components
 export class ShaderSceneSDF implements IShaderScene
@@ -11,6 +14,8 @@ export class ShaderSceneSDF implements IShaderScene
     private scene: Scene = new Scene();
     private visualizer!: ShaderVisualizer;
     private camera!: PerspectiveCamera;
+    private renderer!: WebGLRenderer;
+    private orbitControls: OrbitControls | FreeFlyCamera | undefined;
 
     private debugUI!: DebugUI;
     private textureLoader: TextureLoader = new TextureLoader();
@@ -20,8 +25,39 @@ export class ShaderSceneSDF implements IShaderScene
     private defaultCameraFar: number = 100.0;
     private defaultCameraPos: Vector3 = new Vector3();
 
-    private sdfUniforms: SDFPostProcessingUniforms = {
+    private sphereTarget!: Object3D;
+    private boxTarget!: Object3D;
+    private capsuleTarget!: Object3D;
 
+    private sdfUniforms: SDFPostProcessingUniforms = {
+        u_ScreenResolution: { value: new Vector2() },
+        u_CameraPos: { value: new Vector3() },
+        u_CameraMatrixWorld: { value: new Matrix4() },
+        u_ProjectionMatrixInverse: { value: new Matrix4() },
+
+        u_LightDir: { value: new Vector3 },
+        u_AmbientIntensity: { value: 0.25 },
+        u_SceneColor: { value: new Color(0xffffff) },
+
+        u_SphereData: { value: {
+            pos: new Vector3(0.0, 1.25, 0.0),
+            size: 2.0,
+            color: new Color(0xff0000)
+        }},
+        u_BoxData: { value: {
+            pos: new Vector3(-1.25, -1.25, 0.0),
+            size: 2.0,
+            color: new Color(0x00ff00)
+        }},
+        u_CapsuleData: { value: {
+            pos: new Vector3(1.25, -1.25, 0.0),
+            size: 1.0,
+            color: new Color(0x0000ff)
+        }},
+
+        u_ShapeSmoothness: { value: 0.5 },
+
+        u_DiffuseTex: { value: null }
     }
 
     public getScene() { return this.scene; }
@@ -30,11 +66,15 @@ export class ShaderSceneSDF implements IShaderScene
     {
         this.visualizer = visualizer;
         this.camera = visualizer.cameraManager.getCamera();
+        this.renderer = visualizer.cameraManager.getRenderer();
+        this.orbitControls = visualizer.cameraManager.controls;
 
         //Store current camera properties to be able to reset them later on
         this.defaultCameraFar = this.camera.far;
         this.defaultCameraNear = this.camera.near;
         this.defaultCameraPos.copy(this.camera.position);
+
+        this.camera.position.set(5.0, 5.0, 15.0);
 
         //Set desired camera properties
         this.camera.near = 0.1;
@@ -48,6 +88,13 @@ export class ShaderSceneSDF implements IShaderScene
         let directionalLight = new DirectionalLight(0xffffff, 3.0);
         directionalLight.position.set(10.0, 10.0, 5.0);
         this.scene.add(directionalLight);
+
+        this.sdfUniforms.u_LightDir.value.copy(directionalLight.target.position).sub(directionalLight.position).normalize();
+        this.sdfUniforms.u_SceneColor.value.setStyle((this.visualizer.cameraManager.getScene().background as Color).getStyle());
+
+        this.sphereTarget = this.spawnTransformControls(this.sdfUniforms.u_SphereData.value.pos);
+        this.boxTarget = this.spawnTransformControls(this.sdfUniforms.u_BoxData.value.pos);
+        this.capsuleTarget = this.spawnTransformControls(this.sdfUniforms.u_CapsuleData.value.pos);
 
 
         this.visualizer.cameraManager.usePostProcessing = true;
@@ -70,7 +117,14 @@ export class ShaderSceneSDF implements IShaderScene
 
     public update(deltaTime: number)
     {
-        
+        this.sdfUniforms.u_ScreenResolution.value.set(this.renderer.domElement.width, this.renderer.domElement.height);
+        this.sdfUniforms.u_CameraMatrixWorld.value.copy(this.camera.matrixWorld);
+        this.sdfUniforms.u_ProjectionMatrixInverse.value.copy(this.camera.projectionMatrixInverse);
+        this.sdfUniforms.u_CameraPos.value.copy(this.camera.position);
+
+        this.sdfUniforms.u_SphereData.value.pos.copy(this.sphereTarget.position);
+        this.sdfUniforms.u_BoxData.value.pos.copy(this.boxTarget.position);
+        this.sdfUniforms.u_CapsuleData.value.pos.copy(this.capsuleTarget.position);
     }
 
     public postRender()
@@ -82,6 +136,10 @@ export class ShaderSceneSDF implements IShaderScene
     public hide()
     {
         this.debugUI.reset(); //Events will also unsubscribe here
+
+        this.defaultCameraFar = this.camera.far;
+        this.defaultCameraNear = this.camera.near;
+        this.defaultCameraPos.copy(this.camera.position);
     }
 
 
@@ -89,8 +147,32 @@ export class ShaderSceneSDF implements IShaderScene
     {
         this.debugUI.reset();
 
-        this.defaultCameraFar = this.camera.far;
-        this.defaultCameraNear = this.camera.near;
-        this.defaultCameraPos.copy(this.camera.position);
+        this.debugUI.addFolder("Basic Shapes SDF", "");
+        this.debugUI.addSlider("Basic Shapes SDF", this.sdfUniforms.u_SphereData.value, "size", 0.0, 5.0, "Sphere Radius");
+        this.debugUI.addSlider("Basic Shapes SDF", this.sdfUniforms.u_BoxData.value, "size", 0.0, 5.0, "Box Size");
+        this.debugUI.addSlider("Basic Shapes SDF", this.sdfUniforms.u_CapsuleData.value, "size", 0.0, 5.0, "Capsule Radius");
+        this.debugUI.addSlider("Basic Shapes SDF", this.sdfUniforms.u_ShapeSmoothness, "value", 0.001, 5.0, "Shape Smoothness");
+    }
+
+    private spawnTransformControls(pos: Vector3)
+    {
+        let targetObj = new Object3D();
+        this.scene.add(targetObj);
+
+        let transfControls = new TransformControls(this.camera, this.renderer.domElement);
+        transfControls.attach(targetObj);
+        transfControls.setMode('translate');
+
+        transfControls.addEventListener('dragging-changed', (event) => { this.onTransformControlDragChange(event); });
+        this.scene.add(transfControls);
+
+        targetObj.position.copy(pos);
+        return targetObj;
+    }
+
+    private onTransformControlDragChange(event: any)
+    {
+        if(this.orbitControls && this.orbitControls instanceof OrbitControls)
+                this.orbitControls.enabled = !event.value;
     }
 }
